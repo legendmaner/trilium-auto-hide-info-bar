ee8ff7fa85c2420e255f72314200f8fe90011ba4
legendmaner <legendmaner@gmail.com>
2024-03-28 20:33:28 +0800

    Update trilium- auto-hide-info-bar.js
    adding display_status to make more stable

--- trilium- auto-hide-info-bar.js
+++ trilium- auto-hide-info-bar.js
@@ -6,9 +6,13 @@ version:0.5
 var hidden_title = true; //true:hidden false:display
 var hidden_ribbon = true; //true:hidden false:display
 var pin_ribbon_title = false; //hidden by default
-var delay_execution_time = 300 //The event that the mouse needs to stay when the mouse moves in, default:100ms
+var delay_execution_time = 1500; //The event that the mouse needs to stay when the mouse moves in, default:100ms
 
-
+const HIDDEN = 0;
+const DISPLAYED = 1;
+const HIDING = 2;
+const DISPLAYING = 3;
+let display_status = HIDDEN;
 
 
 if (!hidden_title && !hidden_ribbon) { return }
@@ -21,7 +25,14 @@ var pinButton = function () {
     }
 };
  
-function display() {
+function display(ignore_status = false) {
+    console.log(`display: cur status: ${display_status}`);
+    if (!ignore_status) {
+        if (display_status == DISPLAYED || display_status == HIDING) {
+            return ;
+        }
+        display_status = DISPLAYED;
+    }
     if (hidden_ribbon) {
         $('div.component.note-split:not(.hidden-ext) div.ribbon-container.component').css('display', 'block');
         $('div.component.note-split:not(.hidden-ext) div.shared-info-widget.alert.alert-warning.component').css('display', 'block');
@@ -36,7 +47,14 @@ function display() {
         $('div#rest-pane.component div.tab-row-widget.component').css('border-bottom', '');
     }
 }
-function hidden() {
+function hidden(ignore_status = false) {
+    if (!ignore_status) {
+        if (display_status == HIDDEN || display_status == DISPLAYING) {
+            return ;
+        }
+        display_status = HIDDEN;
+    }
+    
     if (hidden_ribbon) {
         $('div.component.note-split:not(.hidden-ext) div.ribbon-container.component').css('display', 'none');
         $('div.component.note-split:not(.hidden-ext) div.shared-info-widget.alert.alert-warning.component').css('display', 'none');
@@ -81,10 +99,17 @@ class hiddenRibbon extends api.NoteContextAwareWidget {
             $('div.component.note-split:not(.hidden-ext) div.hidden-ribbon-pin.ribbon-tab-title').on("click", pinButton);
             if (pin_ribbon_title==true){
                 $('.hidden-ribbon-pin.ribbon-tab-title-icon.bx').removeClass('hidden').addClass('pin');
+                display(true);
+            } else {
+                hidden(true);
             }
-            display();
+            
             var timeoutEnter,timeoutLeave;
             $("div.component.note-split:not(.hidden-ext) div.component.scrolling-container").mouseenter(function () {
+                if (display_status == HIDDEN || display_status == HIDING) {
+                    return;
+                }
+                display_status = HIDING;
                 clearTimeout(timeoutEnter); 
                 clearTimeout(timeoutLeave);
                 timeoutEnter = setTimeout(function () {
@@ -94,20 +119,38 @@ class hiddenRibbon extends api.NoteContextAwareWidget {
                 }, delay_execution_time); 
             })
             $('div.component.note-split:not(.hidden-ext) div.component.title-row').mouseenter(function (event) {
+                if (display_status == DISPLAYED || display_status == DISPLAYING) {
+                    return;
+                }
+                display_status = DISPLAYING;
                 clearTimeout(timeoutEnter); 
                 clearTimeout(timeoutLeave);
                 timeoutLeave = setTimeout(function () {
                         display();
                     },delay_execution_time);
+            });
+            $('div.component.note-split:not(.hidden-ext) div.component.title-row').mouseleave(function (event) {
+                clearTimeout(timeoutEnter); 
+                clearTimeout(timeoutLeave);
             });
             $('div.tab-row-widget.component').mouseenter(function (event) {
+                if (display_status == DISPLAYED || display_status == DISPLAYING) {
+                    return;
+                }
+                display_status = DISPLAYING;
                 clearTimeout(timeoutEnter); 
                 clearTimeout(timeoutLeave);
                 timeoutLeave = setTimeout(function () {
                         display();
                     },delay_execution_time);
             });
+                
+                
             $('div.component.note-split:not(.hidden-ext) div.ribbon-container.component').mouseenter(function (event) {
+                if (display_status == DISPLAYED || display_status == DISPLAYING) {
+                    return;
+                }
+                display_status = DISPLAYING;
                 clearTimeout(timeoutEnter); 
                 clearTimeout(timeoutLeave);
                 timeoutLeave = setTimeout(function () {
@@ -121,5 +164,3 @@ class hiddenRibbon extends api.NoteContextAwareWidget {
 }
 
 module.exports = new hiddenRibbon();
-
-
